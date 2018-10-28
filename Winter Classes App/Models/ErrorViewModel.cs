9367ebb6ef3c6be298e4143dc1758b2f61b892eb
Patryk <284930@ee.pw.edu.pl>
2018-10-28 18:05:26 +0100

    TASK 7: Init project

---
+++ Winter Classes App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Winter_Classes_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
